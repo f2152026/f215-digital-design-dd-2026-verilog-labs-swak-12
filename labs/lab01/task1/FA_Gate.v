6aac6cb7a2c8f58300d07b3ccc7c66f9ace01639
Swaecha Kapoor <f20240747@pilani.bits-pilani.ac.in>
2026-08-28 13:06:44 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -17,9 +17,10 @@ module FA_Gate(
   wire ps, pc1, pc2;
 
   xor (ps,  a,   b);
-  and (pc1, a,   b);
+  or  (cout, pc1, pc2);
   xor (sum, cin, ps);
   and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  and (pc1, a,   b);
+
 
 endmodule
